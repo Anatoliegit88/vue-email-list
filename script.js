787bb76369bf7762e5d856293a7886ef2e3b9fef
Anatoliegit88 <anatolie.coroban88@gmail.com>
2022-10-28 18:01:15 +0200

    upgrade

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
 const { createApp } = Vue;
 
-
-  createApp({
-    data() {
-      return {
-        numEmail: [],
-      };
-    },
-    created() {
-      this.getEmails();
-    },
-    methods: {
-      getEmails() {
-        for (let i = 0; i < 10; i++) {
-          this.numEmail.push(resp.data.response)
+createApp({
+  data() {
+    return {
+      numEmail: [],
+    };
+  },
+  created() {
+    this.getEmails();
+  },
+  methods: {
+    getEmails() {
+      for (let i = 0; i < 10; i++) {
         axios
           .get("https://flynn.boolean.careers/exercises/api/random/mail")
           .then((resp) => {
-            console.log(resp.data.response);
+            this.numEmail.push(resp.data.response);
           });
-      },
+      }
     },
-  }).mount("#app");
-}
+  },
+}).mount("#app");
